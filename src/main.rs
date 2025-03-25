e49a6cdd1dbb26f981f79d865cffd1616435c737
Vighnesh Sawant <sawantgauri310@gmail.com>
2025-03-25 10:38:20 +0530

    changes

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use rand::Rng;
 fn main(){
   println!("Guess The Number");
   let number = rand::rng().random_range(1..100);
-  
-  
   loop {
     println!("Enter a number:");
     let mut n = String::new();
+  
     io::stdin().read_line(&mut n).expect("Failed to read line");
+    
     let n: u32 = match n.trim().parse() {
-        Ok(num) => num,
+        Ok(n) => n,
         Err(_) => continue
     };
     
